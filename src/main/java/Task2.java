a9446dc1ab4de433e49bb629d82eeb95df0905f7
baeblern <80343544+baeblern@users.noreply.github.com>
2021-10-15 15:29:31 +0400

    task 2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -9,3 +9,15 @@ abstract class Account {
 
     public abstract void subtract(int amount);
 }
+
+class reg_Account extends Account{
+    int amount;
+
+    public void add(int amount){
+        this.amount += amount;
+    }
+    public void subtract(int amount){
+        this.amount -= amount;
+    }
+
+}
